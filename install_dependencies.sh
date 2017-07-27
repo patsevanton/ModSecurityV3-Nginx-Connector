1afe7ab758f8b2af4d1492e382ef02bd1fbf15f9
Anton Patsev <apatsev@luxoft.com>
2017-07-27 18:03:05 +0600

    deleted spectool -g -R nginx.spec in install_dependencies.sh

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -2,6 +2,5 @@
 
 yum -y install gcc-c++ flex bison yajl yajl-devel curl-devel curl GeoIP-devel doxygen zlib-devel libtool \
 autoconf automake libxml2-devel pcre-devel spectool yum-utils rpm-build wget openssl-devel libxslt-devel perl-ExtUtils-Embed
-spectool -g -R nginx.spec
 yum-builddep -y nginx.spec
 
